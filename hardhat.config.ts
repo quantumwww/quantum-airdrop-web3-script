6f2a27bd05e2774ca9b659c76c0c903bac76b1be
Quantum <wwtrendyfy@gmail.com>
2025-07-01 11:30:50 -0700

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,62 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@gelatonetwork/web3-functions-sdk/hardhat-plugin";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "dotenv";
+
+const RPC_PROVIDER = process.env.RPC_PROVIDER;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+
+import assert from "assert";
+assert.ok(RPC_PROVIDER, "Missing RPC_PROVIDER in .env");
+
+const config: HardhatUserConfig = {
+  w3f: {
+    rootDir: "./web3-functions",
+    debug: false,
+    networks: ["ethereum"],
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.21",
+        settings: {
+          optimizer: { enabled: true, runs: 999999 },
+          evmVersion: "paris",
+        },
+      },
+    ],
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: RPC_PROVIDER,
+        blockNumber: 17844146,
+      },
+    },
+    ethereum: {
+      chainId: 1,
+      url: RPC_PROVIDER,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_KEY,
+    },
+  },
+};
+
+export default config;
